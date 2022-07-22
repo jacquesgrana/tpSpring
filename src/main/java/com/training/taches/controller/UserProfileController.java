9f2f8b81c94c656935e1a70158726e25bc4edc60
Jacques Granarolo <jac.granarolo.info@gmail.com>
2022-07-22 16:37:10 +0200

    tp 2e journée après-midi : qualifier

--- src/main/java/com/training/taches/controller/UserProfileController.java
+++ src/main/java/com/training/taches/controller/UserProfileController.java
@@ -5,6 +5,7 @@ import com.training.taches.entity.UserProfile;
 import com.training.taches.service.IUserProfileService;
 import com.training.taches.service.impl.MenProfileService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,66 +17,37 @@ public class UserProfileController {
 
     //UserProfileService userProfileService = new UserProfileService();
     @Autowired
-    IUserProfileService menProfileService;
-
+    @Qualifier("womenProfileService")
+    IUserProfileService userProfileService;
+/*
     @Autowired
     IUserProfileService womenProfileService;
-
+*/
     @RequestMapping(path="all", method = RequestMethod.GET)
-    public List<UserProfile> getAll(@RequestParam String civility) {
-        if (civility.equals("M.")) {
-            List<UserProfile> users = menProfileService.search();
+    public List<UserProfile> getAll() {
+            List<UserProfile> users = userProfileService.search();
             return users;
-        }
-        else {
-            List<UserProfile> users = womenProfileService.search();
-            return users;
-        }
-
     }
 
     @PostMapping
-    public void addUser(@RequestBody UserProfile user, @RequestParam String civility) {
-        if (civility.equals("M.")) {
-            menProfileService.addOne(user);
-        }
-        else {
-            womenProfileService.addOne(user);
-        }
+    public void addUser(@RequestBody UserProfile user) {
+        userProfileService.addOne(user);
     }
 
     @RequestMapping(path = "{id}", method = RequestMethod.GET)
-    public UserProfile findOne(@PathVariable String id, @RequestParam String civility) {
+    public UserProfile findOne(@PathVariable String id) {
         UserProfile user;
-        if (civility.equals("M.")) {
-            user = menProfileService.getOne(id);
-        }
-        else {
-            user = womenProfileService.getOne(id);
-        }
-
+        user = userProfileService.getOne(id);
         return user;
     }
 
     @DeleteMapping(path = "{id}")
-    public void deleteUSer(@PathVariable String id, @RequestParam String civility) {
-        if (civility.equals("M.")) {
-            menProfileService.deleteUser(id);
-        }
-        else {
-            womenProfileService.deleteUser(id);
-        }
-
+    public void deleteUSer(@PathVariable String id) {
+        userProfileService.deleteUser(id);
     }
 
     @PutMapping(path = "{id}")
-    public void updateUser(@RequestBody UserProfile user, @PathVariable String id, @RequestParam String civility) {
-        if (civility.equals("M.")) {
-            menProfileService.updateUser(user, id);
-        }
-        else {
-            womenProfileService.updateUser(user, id);
-        }
-
+    public void updateUser(@RequestBody UserProfile user, @PathVariable String id) {
+        userProfileService.updateUser(user, id);
     }
 }
